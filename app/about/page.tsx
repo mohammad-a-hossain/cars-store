f854244acc9fa8d0d0b6d29d91a309ad46d66be5
mohammad-a-hossain <adam.eve007@gmail.com>
2023-06-13 13:58:45 +0600

    test code

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+ const page = () => {
+  return (
+    <div>abuout page page
+
+        <Link href='/' className='text-red-500'>back</Link>
+    </div>
+  )
+}
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
      well come to the apps
+     
     </main>
   )
 }
